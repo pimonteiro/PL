da223e144eb16dd374ccdf724c11e500ff9f8606
brunomartins <brunocmartins@live.com.pt>
2019-03-25 17:29:22 +0000

    Implements regex.

--- include/article.h
+++ include/article.h
@@ -3,16 +3,16 @@
 
 typedef struct article *Article{
   char*  title;
-  char** info;
+  char* info;
   char*  abstract;
   char** category;
   char*  url;
 };
 
-void add_tilte(Article a, char* t);
-void add_info(Article a, char* t);
-void add_abstract(Article a, char* t);
-void add_category(Article a, char*);
-void add_url(Article a, char* url);
+void add_tilte(Article a, char* t, int size);
+void add_info(Article a, char* t, int size);
+void add_abstract(Article a, char* t, int size);
+void add_category(Article a, char* t, int size);
+void add_url(Article a, char* url, int size);
 
 #endif
