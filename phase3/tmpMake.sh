82f56a0dea951ab3c2fdb19f6f96f98605c5ea57
Filipe Monteiro <pimonteiro@protonmail.com>
2019-06-10 23:50:12 +0100

    final make

--- phase3/tmpMake.sh
+++ phase3/tmpMake.sh
@@ -1,1 +1,1 @@
-gcc -Wall -g -o bin/prog src/evento.c src/lex.yy.c src/pessoa.c src/util.c src/y.tab.c $(pkg-config --cflags --libs glib-2.0)
+make prepare && gcc -Wall -O2 -o bin/prog src/evento.c src/lex.yy.c src/pessoa.c src/util.c src/y.tab.c $(pkg-config --cflags --libs glib-2.0)
